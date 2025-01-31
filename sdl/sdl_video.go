2869367659decf745810479dea8c1e033745454e
Robin Eklind <rnd0x00@gmail.com>
2025-02-01 00:45:57 +0100

    video: add remaining WindowFlags enums

--- sdl/sdl_video.go
+++ sdl/sdl_video.go
@@ -5,8 +5,31 @@ type Window struct{}
 type WindowFlags uint64
 
 const (
-	WindowOpenGL    WindowFlags = 0x0000000000000002
-	WindowResizable WindowFlags = 0x0000000000000020
+	WindowFullscreen        WindowFlags = 0x0000000000000001
+	WindowOpenGL            WindowFlags = 0x0000000000000002
+	WindowOccluded          WindowFlags = 0x0000000000000004
+	WindowHidden            WindowFlags = 0x0000000000000008
+	WindowBorderless        WindowFlags = 0x0000000000000010
+	WindowResizable         WindowFlags = 0x0000000000000020
+	WindowMinimized         WindowFlags = 0x0000000000000040
+	WindowMaximized         WindowFlags = 0x0000000000000080
+	WindowMouseGrabbed      WindowFlags = 0x0000000000000100
+	WindowInputFocus        WindowFlags = 0x0000000000000200
+	WindowMouseFocus        WindowFlags = 0x0000000000000400
+	WindowExternal          WindowFlags = 0x0000000000000800
+	WindowModal             WindowFlags = 0x0000000000001000
+	WindowHighPixelDensity  WindowFlags = 0x0000000000002000
+	WindowMouseCapture      WindowFlags = 0x0000000000004000
+	WindowMouseRelativeMode WindowFlags = 0x0000000000008000
+	WindowAlwaysOnTop       WindowFlags = 0x0000000000010000
+	WindowUtility           WindowFlags = 0x0000000000020000
+	WindowTooltip           WindowFlags = 0x0000000000040000
+	WindowPopupMenu         WindowFlags = 0x0000000000080000
+	WindowKeyboardGrabbed   WindowFlags = 0x0000000000100000
+	WindowVulkan            WindowFlags = 0x0000000010000000
+	WindowMetal             WindowFlags = 0x0000000020000000
+	WindowTransparent       WindowFlags = 0x0000000040000000
+	WindowNotFocusable      WindowFlags = 0x0000000080000000
 )
 
 type WindowID uint32
