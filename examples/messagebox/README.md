d2b38a6e4a4dea27c9073700930f4930109c6ed4
JupiterRider <60042618+JupiterRider@users.noreply.github.com>
2025-02-20 18:32:37 +0100

    messagebox example added

---
+++ examples/messagebox/README.md
@@ -0,0 +1,9 @@
+This is a short example showing SDL's MessageBox API. It creates a dialog box with a title, a message and two buttons.
+
+Linux:
+
+![Screenshot Linux](./messagebox_linux.png)
+
+Windows:
+
+![Screenshot Windows](./messagebox_windows.png)
---
+++ examples/messagebox/main.go
@@ -0,0 +1,37 @@
+package main
+
+import (
+	sdl "github.com/jupiterrider/purego-sdl3/sdl"
+)
+
+func main() {
+	var messageBox sdl.MessageBoxData
+	messageBox.Flags = sdl.MessageBoxInformation
+	messageBox.SetTitle("Question")
+	messageBox.SetMessage("Are you sure?")
+
+	var yesButton sdl.MessageBoxButtonData
+	yesButton.Flags = sdl.MessageBoxButtonReturnkeyDefault
+	yesButton.ButtonID = 1
+	yesButton.SetText("Yes")
+
+	var noButton sdl.MessageBoxButtonData
+	noButton.Flags = sdl.MessageBoxButtonEscapekeyDefault
+	noButton.ButtonID = 2
+	noButton.SetText("No")
+	messageBox.SetButtons(yesButton, noButton)
+
+	var pressedButton int32
+	if !sdl.ShowMessageBox(&messageBox, &pressedButton) {
+		sdl.LogError(sdl.LogCategoryApplication, "%s", sdl.GetError())
+	}
+
+	switch pressedButton {
+	case 1:
+		sdl.Log("User pressed Yes")
+	case 2:
+		sdl.Log("User pressed No")
+	default:
+		sdl.Log("User hasn't pressed any button")
+	}
+}
