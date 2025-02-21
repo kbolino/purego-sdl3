6f069e79b488715e6b46663b7cd397e1d6633056
JupiterRider <60042618+JupiterRider@users.noreply.github.com>
2025-02-21 12:40:39 +0100

    get rid of golang.org/x/sys dependency

--- go.mod
+++ go.mod
@@ -2,7 +2,4 @@ module github.com/jupiterrider/purego-sdl3
 
 go 1.18
 
-require (
-	github.com/ebitengine/purego v0.8.2
-	golang.org/x/sys v0.29.0
-)
+require github.com/ebitengine/purego v0.8.2
--- internal/convert/convert.go
+++ internal/convert/convert.go
@@ -1,13 +1,29 @@
-//go:build !windows
-
 package convert
 
-import "golang.org/x/sys/unix"
+import (
+	"strings"
+	"syscall"
+	"unsafe"
+)
 
+// ToBytePtr converts a Go string into a null-terminated C-style string.
+// It returns an [syscall.EINVAL] error, if s contains a null byte at any location.
 func ToBytePtr(s string) (*byte, error) {
-	return unix.BytePtrFromString(s)
+	if strings.IndexByte(s, 0) != -1 {
+		return nil, syscall.EINVAL
+	}
+	result := make([]byte, len(s)+1)
+	copy(result, s)
+	return &result[0], nil
 }
 
+// ToString converts a null-terminated C-style string into a Go string.
 func ToString(p *byte) string {
-	return unix.BytePtrToString(p)
+	if p == nil {
+		return ""
+	}
+	i := 0
+	for ptr := unsafe.Pointer(p); *(*byte)(unsafe.Add(ptr, i)) != 0; i++ {
+	}
+	return string(unsafe.Slice(p, i))
 }
